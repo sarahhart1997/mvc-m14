61026a72177ca0f7e68a326bee898fc054175dbd
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-25 12:51:23 -0400

    post routes basic

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
+    order: [['created_at', 'DESC']],
     include: [
       {
         model: Comment,
